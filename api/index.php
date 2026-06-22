22a9fb28acbb6fcc9dfa500648c64ed9e58dfdb6
Muhammad Qusyairi <mqusyairi2004@gmail.com>
2026-06-22 10:25:40 -0700

    fix: downgrade runtime php ke versi stabil

--- api/index.php
+++ api/index.php
@@ -1,18 +1,3 @@
 <?php
 
-// Jalankan autoloader bawaan Laravel
-require __DIR__ . '/../vendor/autoload.php';
-
-// Jalankan aplikasi Laravel bootstrap
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// Proses request yang masuk
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
