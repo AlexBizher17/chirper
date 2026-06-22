95ba3346bf024bdbf144c866fab5a1b250ac1454
Muhammad Qusyairi <mqusyairi2004@gmail.com>
2026-06-22 10:15:11 -0700

    fix: update index router dan konfigurasi vercel json

--- api/index.php
+++ api/index.php
@@ -1,2 +1,18 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+// Jalankan autoloader bawaan Laravel
+require __DIR__ . '/../vendor/autoload.php';
+
+// Jalankan aplikasi Laravel bootstrap
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Proses request yang masuk
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
